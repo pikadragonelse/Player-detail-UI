b5fe4cfebf5edcbddb03b9a0064c3d3d2d288d17
pikadragonelse <28072002long@gmail.com>
2025-01-21 10:54:15 +0700

    feat: player info UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { Header } from "./component/header";
+import { InfoSection } from "./component/info-section";
+import { PersonalInfo } from "./component/personal-info/personal-info";
 
 function App() {
     return (
-        <main>
+        <main className="h-full">
             <Header />
+            <div className="bg-[#01040D] h-full flex flex-col gap-6 pt-4 lg:p-8 lg:pt-1">
+                <InfoSection content={<PersonalInfo />} isClearStyleOnMobile />
+                <InfoSection title="Transfer value" />
+                <InfoSection title="Matches" />
+            </div>
         </main>
     );
 }
